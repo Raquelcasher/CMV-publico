8262adb690db38a2dfb0bde6b7ca6ad1757e38fd
Raquelcasher <raquelcasher@hotmail.es>
2021-06-09 22:30:45 +0100

    FIN 09

--- Proyectofinal/ProyectoRaquel/src/app/app.component.ts
+++ Proyectofinal/ProyectoRaquel/src/app/app.component.ts
@@ -15,7 +15,17 @@ export class AppComponent {
       this.now = new Date();
  
     }, 1000);
- 
+  /*FONDO*/
+
+
   }
+      /*switchButton.addEventListener('click', () => {
+        //toggle the HTML body the class 'dark'
+        $("*, p").toggleClass("noche");
+        //$("nav").toggleClass("paranav");
+        switchButton.classList.toggle('active')//toggle the HTML button with the id='switch' with the class 'active'
+      });*/
+  
+      
 
 }
--- Proyectofinal/ProyectoRaquel/src/app/app.module.ts
+++ Proyectofinal/ProyectoRaquel/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MostrarOcultarComponent } from './mostrar-ocultar/mostrar-ocultar.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
 
   declarations: [
     AppComponent,
     MostrarOcultarComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
